6b8d399f1016b619f2bfe4c548b2736e4999d7a6
Gyanendra DS <gyanendra.kalakar@dotsquares.com>
2022-04-05 11:39:55 +0530

    Home page HTML new updates

--- resources/views/layouts/front/header.blade.php
+++ resources/views/layouts/front/header.blade.php
@@ -1,41 +1,72 @@
 <!-- Header Start here -->
 <header class="header-main">
+    <!-- Mobile Top Start here -->
+    <div class="top-bar-mob">
+        <div class="container">
+            <div class="owl-carousel owl-theme mobil-bar">
+                <div class="item">
+                    <a class="whatspp-num" href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i>WhatsApp Help: +44753 5425059</a>
+                </div>
+                <div class="item">
+                    <div class="addr-number">
+                        <p>Birmingham:<a href="tel:01212364415">0121 236 4415</a> | London:<a href="tel:02074051477">020 7405 1477</a></p>
+                    </div>
+                </div>
+                <div class="item">
+                    <p>FINE JEWELLERS SINCE 1951 | TRUSTED BY THOUSANDS</p>
+                </div>
+                <div class="item">
+                    <img src="assets/images/review-one.png" alt="review-one">
+                </div>
+            </div>  
+        </div>
+    </div>
+    <!-- Mobile Top end here -->
+
     <!--Top bar start -->
     <div class="top-bar">
         <div class="container">
             <p>STORE OPEN NOW. OVER 2500 DIAMONDS IN STOCK TO TAKE AWAY AT ONLINE PRICES</p>
         </div>
     </div>
-    <!--Top bar end -->
+    <!--Top bar end --> 
 
     <!--middle Top bar start -->
     <div class="middle-topbar">
         <div class="container">
-            <div class="middle-topbar-wrap flexed justify-between">
+            <div class="middle-topbar-wrap flexed flex-justify-between">
                 <div class="middle-topbar-left">
-                    <ul>
-                        <li>
-                            <a href="tel:+447535425059">
-                                <i class="fa fa-whatsapp" aria-hidden="true"></i> WHATSAPP HELP +447535425059
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="fa fa-map-marker" aria-hidden="true"></i>LOCATIONS
-                            </a>
-                        </li>
-                    </ul>
+                    <div class="whatsapp-top-h">
+                        <a href="tel:+447535425059">
+                            <i class="fa fa-whatsapp" aria-hidden="true"></i> WHATSAPP HELP +447535425059
+                        </a>
+                    </div>
+                    <div class="location-top-h">
+                        <a href="#">
+                            <i class="fa fa-map-marker" aria-hidden="true"></i>LOCATIONS
+                            <div class="location-drop">
+                                <div class="location-drop-inner">
+                                    <p class="location-details-title">Birmingham Details</p>
+                                    <p class="location-text">Birmingham: 0121 236 4415 <br>(Customer Help: 9am - 9pm) </p>
+                                </div>
+                                <div class="location-drop-inner">
+                                    <p class="location-details-title">London Details</p>
+                                    <p class="location-text">London: 0207 4051477  <br> (Customer Help: 9am - 9pm) </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
                 <div class="middle-topbar-right">
                     <ul>
-                        <li>
+                        <li class="my-account-blk">
                             <a href="#"><i class="fa fa-user-o" aria-hidden="true"></i>MY ACCOUNT</a>
                         </li>
-                        <li>
+                        <li class="my-whishlist-blk">
                             <a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i>MY WISHLIST</a>
                         </li>
-                        <li>
-                            <a href="#"><i class="fa fa-shopping-bag" aria-hidden="true"></i>MY CART <span>(0)</span></a>
+                        <li class="my-cart-blk">
+                            <a href="#"><img src="assets/images/cart-color.png" alt="cart"> MY CART <span class="cart-number">(0)</span></a>
                         </li>
                     </ul>
                 </div>
@@ -47,15 +78,44 @@
     <!-- Navbars and logo start here -->
     <div class="logo-menu-bar">
         <div class="container">
-            <div class="logo-menu-bar-wrap flexed justify-between">
+            <div class="logo-menu-bar-wrap flexed flex-justify-between">
 
                 <div class="site-logo-main">
                     <a href="#">
                         <img src="assets/images/marlowsdiamonds-logo.png" alt="site-logo">
                     </a>
                 </div>
+                <div class="mobile-cart-wishlist">
+                    <div class="mobile-wishlist">
+                            <a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+                        </div>
+                    <div class="mobile-cart">
+                        <a href="#"> <img src="assets/images/cart-color-black.png" alt="cart">
+                        <span class="mob-cart-number">0</span>
+                        </a>
+                    </div>
 
+                </div>
                 <div class="main-navigaiton">
+                    <div class="navbar-toggler">
+                        <div class="togglebar-nav">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                    <div class="menus-wraper">
+                        <div class="navbar-toggler">
+                        <div class="togglebar-nav">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                        <div class="mobile-serch-box">
+                            <img src="assets/images/search.png" alt="search">
+                            Search for products...
+                        </div>
                     <nav class="nav-navbars">
                         <ul class="static-megamenu">
                             <li class="level-zero submenu">
@@ -107,9 +167,13 @@
                             </li>
                         </ul>
                     </nav>
+                    </div>
                 </div>
 
                 <div class="head-mini-search">
+                    <div class="remve-mobile-serch-box">
+                        <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                    </div>
                     <form>
                         <div class="formgroup">
                             <input type="text" name="search" placeholder="Search for product..">
@@ -126,7 +190,7 @@
 
     <div class="post-bar">
         <div class="container">
-            <div class="post-bar-wraper flexed justify-between items-center">
+            <div class="post-bar-wraper flexed flex-justify-between flex-items-center">
                 <div class="post-bar-left">
                     <p>Fine Jewellers Since 1951</p>
                 </div>  
