d5cf03977210e0d5023a567f228d9fad79f796c0
sunil <sunil.kumar1@dotsquares.com>
2022-04-20 17:48:25 +0530

    accounts pages

--- app/Http/Controllers/Front/LoginController.php
+++ app/Http/Controllers/Front/LoginController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
 
 class LoginController extends Controller
 {
     public function index()
     {
+        if(Auth::check()){
+            return view('front.loginpages.dashboardpage');
+        }
         return view('front.loginpages.loginpage');
     }
 }
--- resources/views/front/loginpages/loginpage.blade.php
+++ resources/views/front/loginpages/loginpage.blade.php
@@ -74,6 +74,8 @@
         </div>
     </div>
 
+
+    
 @endsection
   
 @section('js')
