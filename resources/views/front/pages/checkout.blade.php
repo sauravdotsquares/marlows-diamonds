7031ca5ecb043918b07032d08a818b08bef92aff
Vadim Ansari <vadim.ansari@dotsquares.com>
2022-05-04 14:27:19 +0530

    company name is optional

--- resources/views/front/pages/checkout.blade.php
+++ resources/views/front/pages/checkout.blade.php
@@ -579,9 +579,9 @@
             last_name: {
                 required: true,
             },
-            company_name: {
-                required: true,
-            },
+            // company_name: {
+                // required: true,
+            // },
             country_id: {
                 required: true,
             },
