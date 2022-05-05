dafdce5982ef2d546c58eaf280611c2dc7615921
Gyanendra DS <gyanendra.kalakar@dotsquares.com>
2022-05-05 11:38:38 +0530

    Resolve JS issue

--- resources/views/front/pages/reviews.blade.php
+++ resources/views/front/pages/reviews.blade.php
@@ -17,15 +17,3 @@
 	</div>
 </div>
 @endforeach
-
-@section('js')
-<script>
-$(document).ready(function(){
-    $('.show-more-content').hide();
-    $('.show-more').click(function(){
-        $(this).parents('.reviewr-review-text').toggleClass("show-text-col");       
-    });
-});
-
-</script>
-@endsection
