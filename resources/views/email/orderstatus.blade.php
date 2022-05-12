5b4815c42790931b28c61ef3054504c451a5e743
Vadim Ansari <vadim.ansari@dotsquares.com>
2022-05-12 19:35:18 +0530

    added email for admin order status change

---
+++ resources/views/email/orderstatus.blade.php
@@ -0,0 +1,127 @@
+<?php 
+// echo "sdfdadsdadfs<pre>"; 
+// print_r($data1['data']['status_details_designs']);
+// die;
+
+?>
+
+	<?php 
+	//print_r($getOrderDetailsMail);
+	//die;
+
+
+?>
+
+<div class="warp" style="max-width:600px; margin:0 auto">
+<table bgcolor="#8e2e65" width="100%" border="0" align="center" cellpadding="0" cellspacing="0" style="font-family:Arial, Helvetica, sans-serif; font-size:15px; color:#221f1f"  class="deviceWidth">
+  <tr>
+    <td align="center" valign="middle"><table width="100%" border="0" cellspacing="0" cellpadding="0" class="deviceWidth">
+      <tr>
+        <td bgcolor="#fff" align="center" style="border:1px solid #8e2e65"><table width="100%" border="0" cellspacing="0" cellpadding="0"  class="centertext">
+          <tr>
+            <td bgcolor="#fff" style="border-bottom: 2px solid #8e2e65;">
+              <table width="45%" border="0" align="left"  cellpadding="0" cellspacing="0" class="deviceWidth centertext">
+                <tr>
+                  <td >
+				  <img src="{{asset('')}}assets/images/logo.png" style="width: 150; margin:15px;"/></td>
+                </tr>
+              </table>
+              <table width="52%" border="0" align="left" cellpadding="0" cellspacing="0" class="deviceWidth centertext">
+    <tbody>
+      <tr>
+        <td valign="top" style="text-align:right;" class="centertext"><span style="font-size:13px; line-height:16px;  color:#fff;" class="centertext"><br>
+            </td>
+      </tr>
+      <tr>
+        <td valign="top" style="text-align:right;" class="centertext"> </td>
+      </tr>
+    </tbody>
+</table>
+            </td>
+            </tr>
+          <tr>
+            <td align="center" valign="top" bgcolor="#F0CA4B"> </td>
+          </tr>
+          <tr>
+            <td align="center" valign="top"><table width="90%" border="0" cellspacing="0" cellpadding="0" class="deviceWidth3">
+              <tr>
+                <td height="30"> </td>
+                </tr>
+              <tr>
+                <td ><p>Hello,</p>
+				
+				
+				<div style="text-align:left;line-height: 24px;">
+					
+						<p>
+                            Order #<mark class="order-number">{{isset($data1['data']['token'])?$data1['data']['token']:''}}</mark> was placed on <mark class="order-date">{{isset($data1['data']['created_at'])?$data1['data']['created_at']:''}}</mark> and is currently 
+                            <!-- <mark class="order-status">Cancelled</mark>  -->
+                           {!!isset($data1['data']['status_details_designs'])?$data1['data']['status_details_designs']:''!!}
+                        </p>
+					
+				</div>
+				
+				
+				
+				</td>
+              </tr>
+              <tr>
+                <td> </td>
+              </tr>
+              
+              <tr>
+                <td height="30"> </td>
+              </tr>
+              <tr>
+                <td> </td>
+              </tr>
+              </table></td>
+            </tr>
+          </table></td>
+      </tr>
+      <tr>
+        <td align="center"> </td>
+      </tr>
+      <tr>
+        <td align="center"><table width="80%" border="0" cellspacing="0" cellpadding="0" class="deviceWidth3">
+          <tr>
+            <td align="center" valign="middle"> </td>
+          </tr>
+          <tr>
+            <td align="center" style="font-size:13px; line-height:16px;"> </td>
+          </tr>
+          <tr>
+            <td align="center" valign="middle" style="font-size:13px; font-family:Arial, Helvetica, sans-serif; line-height:21px;"> </td>
+            </tr>
+          </table></td>
+      </tr>
+    </table>
+
+
+
+<table class="deviceWidth" border="0" width="600" cellspacing="0" cellpadding="0" align="center">
+<tbody>
+<tr>
+<td style="padding: 20px 15px;" bgcolor="#8e2e65">
+<table border="0" width="200px" cellspacing="0" cellpadding="0" align="center">
+<tbody>
+
+</tbody>
+</table>
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="font-size: 14px; font-weight: normal; padding: 10px 0px; line-height: 20px; color: #fff;" align="center">© Copyright 2020 | marlows-diamonds.co.uk | All rights reserved
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</table>
+</div>
+
