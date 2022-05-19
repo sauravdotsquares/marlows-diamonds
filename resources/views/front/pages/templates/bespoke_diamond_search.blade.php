a4683e26c216ac4d98bf4acb2ce82d6d46d327c8
Gajendra <sharma.gajendra@dotsquares.com>
2022-05-19 17:24:58 +0530

    Carat bug fix

--- resources/views/front/pages/templates/bespoke_diamond_search.blade.php
+++ resources/views/front/pages/templates/bespoke_diamond_search.blade.php
@@ -633,7 +633,7 @@
             step: 0.2, // Slider moves in increments of '10'
             margin: 1.0, // Handles must be more than '10' apart
 		    connect: true,
-            behaviour: "fixed", // Move handle on tap, bar is draggable "drag", "tap", "snap", "fixed"
+            behaviour: "drag", // Move handle on tap, bar is draggable "drag", "tap", "snap", "fixed"
 		    tooltips: [true, wNumb({decimals: 0})],
 		    range: {
 		        'min': [0.3],
