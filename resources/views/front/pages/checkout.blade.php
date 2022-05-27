11bbd7b279256d65b912f1a1911a6839a48dba1b
gajendra3036 <sharma.gajendra@dotsquares.com>
2022-05-27 08:35:54 +0000

    local commit

--- resources/views/front/pages/checkout.blade.php
+++ resources/views/front/pages/checkout.blade.php
@@ -340,7 +340,6 @@
         $('#cust_email').on('blur',function(){
             var data = '{!! isset(auth()->user()->email)?auth()->user()->email:'' !!}';
             if(data){
-                //console.log("if");
             }else{
                 if($(this).val() != ''){
                     getEmailCheck();
@@ -363,20 +362,11 @@
                 },
                 success: function (response) {
                     $('#cust_email-error').remove();
-                    // console.log(response);
                     if(response){
                         $('#emailCheck').append('<label id="cust_email-error" class="error" for="cust_email">Email('+customeremail+') is already exist Please Logged in </label>');
                         $('#cust_email').val(" ");
-                        // $('#cust_email-error').css('display','block');
-                        // toastr.success('Already exists please login');
-                        // window.location.reload();
                         return response;
                     }
-                    // else{
-                    //     $('#cust_email-error').remove();
-                    //     // toastr.info('not exist do continue');
-                    //     return response;
-                    // }
                 }
             });
         }else{
@@ -543,23 +533,16 @@
                     $('.cc_place_order_btn button').text('Place Order');
                     $('.cc_place_order_btn button').prop('disabled', false);
                     if(response.status == 500){
-
-
                         $('#emailCheck').append('<label id="cust_email-error" class="error" for="cust_email">Email is already exist. Please try with another email.</label>');
                         toastr.info(response.msg);
                     }
                     if(response.status == 200){
-
-                        // toastr.success(response.msg);
-                        // window.location.reload();
                         if($('#selected_payment_type').val() == 'paypal'){
                             window.location.href = "{{route('make.payment')}}/"+response.order_dt;
                         }else{
                             window.location.href = "{{route('make.dekopay')}}/"+response.order_dt;
                         }
 
-                    }else{
-                        // toastr.info(response.error);
                     }
                 }
             });
