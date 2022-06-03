5b902b1e4b33bf213b40b37c2e6e7e7f6e86ce32
gajendra3036 <sharma.gajendra@dotsquares.com>
2022-06-03 06:25:18 +0000

    For local changes

--- config/database.php
+++ config/database.php
@@ -74,8 +74,6 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => false,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
