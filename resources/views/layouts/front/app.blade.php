4c333c38bdd9b8346546b7fe94a4ddf38f12c687
gajendra3036 <sharma.gajendra@dotsquares.com>
2022-06-14 10:15:58 +0000

    Local changes

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -41,17 +41,16 @@
         display: none !important;
       }
     </style>
-    {!!$header_settings->get_options('google_tag_manager_code')!!}
-    {!!$header_settings->get_options('google_analytics_code')!!}
+   
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-1365164-4"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-1365164-1"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-1365164-4');
+    gtag('config', 'UA-1365164-1');
     </script>
 
 </head>
