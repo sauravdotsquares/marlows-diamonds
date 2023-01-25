a8b08ff8eff55bca572e95f59dc49bd397727e00
Amit kumar <amit.kumar1@dotsquares.com>
2023-01-25 11:18:30 +0530

    remove slash from last of url

--- resources/views/front/sitemap.blade.php
+++ resources/views/front/sitemap.blade.php
@@ -5,7 +5,7 @@
     {{-- Products --}}
     @foreach ($products as $product)
         <url>
-            <loc>{{  env('APP_ROOT_URL') . '/product/'. rtrim($product->slug,'/') }}</loc>
+            <loc>{{  rtrim(env('APP_ROOT_URL') . '/product/'. $product->slug,'/') }}</loc>
             <lastmod>{{ $product->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.8</priority>
         </url>
@@ -14,7 +14,7 @@
     {{-- Posts --}}
     @foreach ($posts as $post)
         <url>
-            <loc>{{  env('APP_ROOT_URL') . '/blog/' . rtrim($post->slug,'/') }}</loc>
+            <loc>{{  rtrim(env('APP_ROOT_URL') . '/blog/' . $post->slug,'/') }}</loc>
             <lastmod>{{ $post->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.8</priority>
         </url>
@@ -23,7 +23,7 @@
     {{-- posts_category --}}
     @foreach ($posts_categories as $posts_category)
         <url>
-            <loc>{{  env('APP_ROOT_URL') . '/blog/category/' . rtrim($posts_category->slug,'/') }}</loc>
+            <loc>{{  rtrim(env('APP_ROOT_URL') . '/blog/category/' . $posts_category->slug,'/') }}</loc>
             <lastmod>{{ $posts_category->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.8</priority>
         </url>
@@ -32,7 +32,7 @@
     {{-- pages --}}
     @foreach ($pages as $page)
         <url>
-            <loc>{{ env('APP_ROOT_URL') .'/' .  rtrim($page->slug,'/') }}</loc>
+            <loc>{{ rtrim(env('APP_ROOT_URL') .'/' .  $page->slug,'/') }}</loc>
             <lastmod>{{ $page->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.8</priority>
         </url>
@@ -41,7 +41,7 @@
     {{-- All other pages --}}
     @foreach ($otherPages as $otherPage)
         <url>
-            <loc>{{ env('APP_ROOT_URL') .'/' .  rtrim($otherPage , '/') }}</loc>
+            <loc>{{ rtrim(env('APP_ROOT_URL') .'/' .  $otherPage , '/') }}</loc>
             <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.8</priority>
         </url>
