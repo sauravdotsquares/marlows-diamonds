14e3a4b83e45635b5e3cdad4bec8e2e3d7d34292
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-03-24 12:51:06 +0530

    For css changes

--- resources/views/layouts/front/header.blade.php
+++ resources/views/layouts/front/header.blade.php
@@ -263,13 +263,13 @@
                     {{-- <a href="{{ route('products.exclusive') }}" >
                         <span> Exclusive to Marlows </span>
                     </a> --}}
-                    <p>Mid Season Sale - Up to 30% off </p> 
-                    <p> 
+                    {{-- <p>Mid Season Sale - Up to 30% off </p>  --}}
+                    <p>
                     {{-- <span class="header-heighlight-text">Up to 30% off</span> --}}
-                         <span class="header-heighlight-text discount_timer"></span> 
-                        <br> 
-                     
-                    </p> 
+                         <span class="header-heighlight-text discount_timer"></span>
+                        <br>
+
+                    </p>
                 </div>
                 <div class="post-bar-right header-post-bar-left">
                     <p>{!!$header_settings->get_options('header-right')!!}</p>
@@ -290,25 +290,25 @@
 
         var now = new Date().getTime();
         var timeleft = countDownDate - now;
-            
+
         // Calculating the days, hours, minutes and seconds left
         var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
         var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-            
+
         // Result is output to the specific element
         const daysToShow = days + "d "
-        const hoursToShow = hours + "h " 
-        const minutesToShow = minutes + "m " 
-        const secondsToShow = seconds + "s " 
+        const hoursToShow = hours + "h "
+        const minutesToShow = minutes + "m "
+        const secondsToShow = seconds + "s "
 
         const timerToShow = daysToShow+' '+ hoursToShow+' '+ minutesToShow+' '+ secondsToShow;
         $(".discount_timer").css('display','inline-block');
         $(".discount_timer").text(timerToShow);
         // console.log('first', hoursToShow);
         // console.log('first', minutesToShow);
-        // console.log('first', secondsToShow);  
+        // console.log('first', secondsToShow);
         // Display the message when countdown is over
         if (timeleft < 0) {
             clearInterval(myfunc);
