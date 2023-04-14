52ed25cc62b251057976b6be4bde9af1808d41b5
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-04-14 10:23:37 +0530

    Mail Format setup

--- resources/views/email/orderstatus.blade.php
+++ resources/views/email/orderstatus.blade.php
@@ -276,7 +276,7 @@
 											</td>
 											<td align="right" style="font-family:Arial; border-bottom: 1px dashed #808080; padding: 15px 0;
 											color: #505050;line-height: 20px; font-size: 14px;">
-													{{MY_CURRENCY_SYMBOL}}{{$data1['data']['final_price']}}
+													{{MY_CURRENCY_SYMBOL}}{{$data1['data']['total_price']}}
 											</td>
 										</tr>
 
@@ -309,7 +309,7 @@
 											</td>
 											<td align="right" style="font-family:Arial; border-bottom: 1px dashed #808080; padding: 15px 0;
 											color: #505050;line-height: 20px; font-size: 14px;">
-													{{MY_CURRENCY_SYMBOL}}{{$data1['data']['final_price']}}
+													{{MY_CURRENCY_SYMBOL}}{{$data1['data']['total_price']}}
 
                                                     {{-- (includes £329.06 VAT) --}}
 											</td>
