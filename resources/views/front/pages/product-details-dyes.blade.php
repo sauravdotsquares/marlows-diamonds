0ca2cde027594b7bcd50e7a9a55ce7832a209222
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-04-21 15:05:53 +0530

    Deko pay functionality for all product details page

--- resources/views/front/pages/product-details-dyes.blade.php
+++ resources/views/front/pages/product-details-dyes.blade.php
@@ -298,7 +298,7 @@
 					<a class="store-locator store-locator-border-right" href="{{asset('visit-us')}}">Store Locator</a>
 					<a target="_blank" id="productCertificateLink" class="view-certificate mined-certificate" href="#">View Certificate</a>
 				</div>
-				{{-- <div class="finance-available" ng-controller="DekopayController">
+				<div class="finance-available" ng-controller="DekopayController">
 					<a href="javascript:void(0)" ng-click="financeOptions()">
 						<i class="fa fa-credit-card" aria-hidden="true"></i>
 						<p>Finance Available
@@ -308,7 +308,7 @@
 					<div class="doko-img">
 						<img src="{{asset('')}}assets/images/Deko_square_colour_whiteBG200px_wide.png" alt="doko">
 					</div>
-				</div> --}}
+				</div>
 
 			</div>
 		</div>
@@ -538,7 +538,7 @@
 
 
 <!-- Modal -->
-{{-- @include('front.includes.dekopay-finance-options') --}}
+@include('front.includes.dekopay-finance-options')
 
 
 
