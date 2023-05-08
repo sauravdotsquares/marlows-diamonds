aad9ab36afda81ffc563872e03011965ad1886bf
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-05-08 18:20:51 +0530

    For sidbar moved on left side in product listing pages

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -486,9 +486,15 @@ class ProductController extends Controller
 
                         $explode_attr = explode('|', $attribute['values']);
 
+                        if(isset($request->diamond_type) && $request->diamond_type == 'mined_diamond'){
+                            $arr_2 = "9ct";
+                            $explode_attr = array_filter($explode_attr, function($value) use ($arr_2) {
+                                return stripos($value, $arr_2) === false;
+                            });
+                        }
                         $getAttrVals = ProductVariationDetails::whereIn('variation_id', $variation_ids)->where('key', 'attri_' . $attribute['slug'])->pluck('value')->toArray();
 
-
+                        
                         $found = [];
                         foreach ($explode_attr as $num) {
                             if (in_array(trim($num), $getAttrVals)) {
