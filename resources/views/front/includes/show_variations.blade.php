c8f12b7abafd3f77f01bfae7be8f78c9b605c748
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-09-04 18:02:41 +0530

    For Ring guide branch

--- resources/views/front/includes/show_variations.blade.php
+++ resources/views/front/includes/show_variations.blade.php
@@ -1,6 +1,11 @@
 
 <div class="type-variations-col">
-    <label for="{{$final_attr['slug']}}">{{$final_attr['name']}}</label>
+    <label for="{{$final_attr['slug']}}">{{$final_attr['name']}}  
+        @if($final_attr['slug'] == 'finger-size')
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#myModal">Guide
+            </a>
+        @endif
+    </label>
 
     <select name="{{$final_attr['slug']}}" id="{{$final_attr['slug']}}" class="form-control">
         @foreach($final_attr['attri_'.$final_attr['slug']] as $key=>$attr)
