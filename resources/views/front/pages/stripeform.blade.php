0d4dedcc84a411230f122eff1f5e9c38856290d1
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-09-14 18:10:37 +0530

    For image setup and markup library changed

--- resources/views/front/pages/stripeform.blade.php
+++ resources/views/front/pages/stripeform.blade.php
@@ -29,7 +29,8 @@
                             </div>
                             <div class='col-xs-12 col-md-6 form-group required cardnumberdiv'>
                                 <label class='control-label'>Card Number</label>
-                                <input autocomplete='off' class='form-control card-number' id="card_number" name="card_number" required size='20' type='text'> <img class="card-images" src="{{asset('assets/images/cards.png')}}">
+                                <img class="card-images" src="{{asset('assets/images/cards.png')}}">
+                                <input autocomplete='off' class='form-control card-number' id="card_number" name="card_number" required size='20' type='text'> 
                             </div>
                         </div>
                         <div class='form-row row'>
