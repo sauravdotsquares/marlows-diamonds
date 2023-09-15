2db18b840cee91595f65f47b72919c918e174cb2
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-09-15 18:35:45 +0530

    For checkout payment design done

--- resources/views/front/pages/payments/paypal.blade.php
+++ resources/views/front/pages/payments/paypal.blade.php
@@ -2,9 +2,9 @@
     <input type="radio" name="payment_type" checked required="required" value="paypal">
     <label class="paypal_label">
         Paypal
-        <img src="{{asset('')}}assets/images/paypal-icon.png" alt="paypal">
         <a class="what-paypal" href="https://www.paypal.com/gb/webapps/mpp/paypal-popup" onclick="javascript:window.open('https://www.paypal.com/gb/webapps/mpp/paypal-popup','WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;">What is PayPal?</a>
     </label>
+    <img src="{{asset('')}}assets/images/paypal-icon.png" alt="paypal">
     <div class="payment-box-main-drop paypal-pay-box">
         Pay via PayPal; you can pay with your credit card if you don’t have a PayPal
         account.
