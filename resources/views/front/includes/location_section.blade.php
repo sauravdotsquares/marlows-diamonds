1a1c0c53a440818d86e6082124e1571e585398e4
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-09-25 16:04:15 +0530

    For product listing css changes

--- resources/views/front/includes/location_section.blade.php
+++ resources/views/front/includes/location_section.blade.php
@@ -1,6 +1,6 @@
 <div class="location-container">
     <div class="location-left-section">
-        <h1 class="location-header">We are here to help <br> call <strong> 020 7405 1477</strong> or <strong> 0121 236 4415</strong> </h1>
+        <h1 class="location-header">We are here to help <br> call <strong> <a href="tel:74051477"> 7405 1477</a></strong> or <strong> <a href="tel:01212364415">0121 236 4415</a></strong> </h1>
         <p class="location-description">Visit us at
             <strong> 20 Beauchamp Pl, Knightsbridge, London SW3 1NQ </strong> and <strong> 46 Warstone Ln, Hockley, Birmingham B18 6JJ </strong> </p>
         <p class="location-button">
