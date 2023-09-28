ba070af0291e0fd54478ad3ffcb99ea0c6729a16
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-09-28 14:20:45 +0530

    For resolved conflicts changes

--- resources/views/layouts/front/header.blade.php
+++ resources/views/layouts/front/header.blade.php
@@ -148,7 +148,6 @@
                     <a href="{{url('/')}}" title="{{$header_settings->get_options('site_title')}}">
                         @if($header_settings->get_options('logo')!='')
                             <img src="{{asset('images/logo/'.$header_settings->get_options('logo'))}}" alt="{{$header_settings->get_options('site_title')}}">
-                            
                         @elseif($header_settings->get_options('site_title')!='')
                             <div>{{$header_settings->get_options('site_title')}}</div>
                             <span>{{$header_settings->get_options('site_tagline')}}</span>
