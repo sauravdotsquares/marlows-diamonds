935a959a626163d624dd9ba4035fa97b0dc296d1
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-10-31 13:05:06 +0530

    For search box hide and show in header

--- public/assets/js/controllers/app.js
+++ public/assets/js/controllers/app.js
@@ -12,21 +12,18 @@ var base_url = systemBaseUrl + 'api/v1/';
 MarlowsAPP.controller("CommonController",function($scope, $http,$compile) {
     
     $scope.searchProducts = function(){
-        // console.log($scope.search);
+        //console.log($scope.search);
         var url  = base_url+"searchProducts";
-        if($scope.search){
-            $http({
-                method  : 'POST',
-                url     : url,
-                data    : {name:$scope.search}
-            }).success(function(data) {
-                    // console.log(data);
-                    $('.search-suggestion').show();
-                    $scope.searchResults = data;
-            });
-        }else{
-            $('.search-suggestion').hide();
-        }
+        $http({
+            method  : 'POST',
+            url     : url,
+            data    : {name:$scope.search}
+
+        }).success(function(data) {
+           // console.log(data);
+            $scope.searchResults = data;
+            
+        });
     }
 
    
