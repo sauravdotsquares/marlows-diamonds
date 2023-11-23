df3a37ea49664c8196888788aa8c6d18054ffcc5
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2023-11-23 19:03:26 +0530

    Navigation menu with images and alternate link dynamically

--- resources/views/layouts/front/menus-sub.blade.php
+++ resources/views/layouts/front/menus-sub.blade.php
@@ -1,20 +1,57 @@
 <ul class="inner-submenu">
-	@foreach($subs as $sub)
-
-        <li class="level-one {{$sub['class_level']}}">
-			<span>
-        		<a href="{{url($sub['href'])}}">{!!$sub['text']!!}
-        		
-        	</a>
-			@if(isset($sub['children']) && count($sub['children']) > 0)
-				<i class="fa fa-angle-right {{$sub['class_level']}}" aria-hidden="true"></i>
-        	@endif
-			
-			</span>
-        	@if(isset($sub['children']) && count($sub['children']) > 0)
-	            @include('layouts.front.menus-sub', ['subs' => $sub['children']])
-	        @endif
-
-        </li>
-    @endforeach
+	@foreach($subs as $keyAdd => $sub)
+		@if($count == 1)
+			@if($keyAdd == 0)
+			<ul> 
+				@if($sub['title'] == 'main')
+					<li class="level-one level-1">
+						<a href="{{url($sub['href'])}}">
+							{!! $sub['image_link'] !!}
+							{{$sub['text']}}
+						</a>	
+					</li>
+				@endif
+				<li class="level-one level-1"> <h4>By Style</h4></li>
+			@endif
+				
+				@if($sub['title'] == 'style')
+					<li class="level-one level-1">
+						<a href="{{url($sub['href'])}}">
+							{!! $sub['image_link'] !!}
+							{{$sub['text']}}
+						</a>	
+					</li>
+				@endif
+			@if($keyAdd == 5)
+				</ul>
+			<ul>
+				<li class="level-one level-1"> <h4 class="shapestyledesign-section">By Shape</h4> </li>
+				@endif
+				@if($sub['title'] == 'shape')
+				<li class="level-one level-1">
+						<a href="{{url($sub['href'])}}">
+							{!! $sub['image_link'] !!}
+							{{$sub['text']}}
+						</a>	
+				</li>
+				@endif
+				@if($keyAdd == 12)
+			</ul>
+			@endif
+		@else
+			<li class="level-one {{$sub['class_level']}}">
+				<span>
+					<a href="{{url($sub['href'])}}">
+						{!!$sub['text']!!}
+					</a>
+					@if(isset($sub['children']) && count($sub['children']) > 0)
+						<i class="fa fa-angle-right {{$sub['class_level']}}" aria-hidden="true"></i>
+					@endif
+				</span>
+				@if(isset($sub['children']) && count($sub['children']) > 0)
+					@include('layouts.front.menus-sub', ['subs' => $sub['children']])
+				@endif
+			</li>
+		@endif
+	@endforeach
 </ul>
