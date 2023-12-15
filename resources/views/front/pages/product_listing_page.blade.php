3e8b8e56390a94fcde1ca62a4596b46c3939e87c
Aman <aman.yadav@dotsquares.com>
2023-12-15 16:25:32 +0530

    images optimization

--- resources/views/front/pages/product_listing_page.blade.php
+++ resources/views/front/pages/product_listing_page.blade.php
@@ -424,7 +424,7 @@
 
 @include('front.includes.instagram-section')
 <div class="engagement-ring-img">
-    <img src="{{env('APP_IMAGE_URL').'/images/viewguide.PNG'}}" alt="Find the perfect engagement ring">
+    <img src="{{getImageOptimizeDetails('/assets/images/banner-hand.jpg','695','696')}}" alt="banner-hand">
     <div class="engagement-ring-img-content">
         <div class="container">
     <h2>Find the perfect engagement ring</h2>
