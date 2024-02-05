757cc010facd2611482d85971f3dca14222af4b5
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2024-02-05 18:21:38 +0530

    Content change of the finance calculator

--- resources/views/front/includes/dekopayformulacalculationpage.blade.php
+++ resources/views/front/includes/dekopayformulacalculationpage.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Finance Options</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Finance Calculator</h5>
             </div>
             <div class="modal-body">
                 <div class="col-lg-12">
