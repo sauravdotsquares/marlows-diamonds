6a7421620e750adce643a115a612e55c8ee0e109
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2024-03-29 15:45:17 +0530

    For dispatch the job and queue mail systems

--- app/Http/Controllers/Front/MailListFormController.php
+++ app/Http/Controllers/Front/MailListFormController.php
@@ -3,9 +3,10 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Mail;
+use Illuminate\Support\Facades\Mail;
 use App\Models\Enquiries;
 use App\Models\Settings;
+use App\Jobs\SendEmailJob;
 
 class MailListFormController extends Controller {
 
@@ -37,17 +38,28 @@ class MailListFormController extends Controller {
     			$message->bcc('sharma.gajendra@dotsquares.com', 'Admin')->subject('New Website Inquiry');
             });
         }else{
-            Mail::send('email.mail', array(
+            $requestData = [
                 'title' => $request->get('title'),
                 'email' => $request->get('email'),
                 'phone' => $request->get('phone'),
-                'url' => $request->get('custom_url'),
-                'user_query' => $request->get('description'),
-            ), function($message) use ($request,$admin_email ){
-                $message->from('hello@marlows-diamonds.co.uk');
-    			$message->to('sharma.gajendra@dotsquares.com', 'Admin')->subject('New Website Inquiry local');
-    			$message->cc('gajendra30@gmail.com', 'Admin')->subject('New Website Inquiry local');
-            });
+                'custom_url' => $request->get('custom_url'),
+                'description' => $request->get('description'),
+            ];
+            
+            $adminEmail = 'sharma.gajendra@dotsquares.com';
+            
+            SendEmailJob::dispatch($requestData, $adminEmail);
+            // Mail::send('email.mail', array(
+            //     'title' => $request->get('title'),
+            //     'email' => $request->get('email'),
+            //     'phone' => $request->get('phone'),
+            //     'url' => $request->get('custom_url'),
+            //     'user_query' => $request->get('description'),
+            // ), function($message) use ($request,$admin_email ){
+            //     $message->from('hello@marlows-diamonds.co.uk');
+    		// 	$message->to('sharma.gajendra@dotsquares.com', 'Admin')->subject('New Website Inquiry local');
+    		// 	//$message->cc('gajendra30@gmail.com', 'Admin')->subject('New Website Inquiry local');
+            // });
         }
        
         return response()->json(['status'=> 200, 'success'=>'Thank you for subscribe us!!!']);
