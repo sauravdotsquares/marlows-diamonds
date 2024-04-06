6afc4d9e19892518b552e626991f84d598fb06a2
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2024-04-06 16:22:48 +0530

    For apply the email templates and images related problems in product details page and product listing pages

--- resources/views/front/ajax/productlistajax.blade.php
+++ resources/views/front/ajax/productlistajax.blade.php
@@ -51,23 +51,23 @@
 			</div>
 			<div class="product-items-item-details">
 				<div class="product-items-item-name">
-
-					<?php 
-						$titleSplits = [];
-						if(isset($product->title) && !empty($product->title)){
-							$titleSplits = explode('|',$product->title);
-						}
-					?>
-					@if(isset($product->slug) && !empty($product->slug))
-						<a href="{{asset('product/'.$product->slug)}}" class="title-list-heading">{{isset($titleSplits[0])?mb_convert_case($titleSplits[0], MB_CASE_TITLE, 'UTF-8'):''}}</a>
-						@if(isset($titleSplits[1]) && !empty($titleSplits[1]))
-							<a href="{{asset('product/'.$product->slug)}}">{{$titleSplits[1]}}</a>
+					<div class="list_product_title">
+						<?php 
+							$titleSplits = [];
+							if(isset($product->title) && !empty($product->title)){
+								$titleSplits = explode('|',$product->title);
+							}
+						?>
+						@if(isset($product->slug) && !empty($product->slug))
+							<a href="{{asset('product/'.$product->slug)}}" class="title-list-heading">{{isset($titleSplits[0])?mb_convert_case($titleSplits[0], MB_CASE_TITLE, 'UTF-8'):''}}</a>
+							@if(isset($titleSplits[1]) && !empty($titleSplits[1]))
+								<a href="{{asset('product/'.$product->slug)}}">{{$titleSplits[1]}}</a>
+							@endif
+						@else
+							<a href="#">{{isset($titleSplits[0])?$titleSplits[0]:''}}</a>
+							<a href="#">{{isset($titleSplits[1])?$titleSplits[1]:''}}</a>
 						@endif
-					@else
-						<a href="#">{{isset($titleSplits[0])?$titleSplits[0]:''}}</a>
-						<a href="#">{{isset($titleSplits[1])?$titleSplits[1]:''}}</a>
-					@endif
-					
+					</div>
 					<?php if(!empty($getProductListingPrices) && $getProductListingPrices != 0){ ?>
 						<div class="price-section">
 							<div style="display: flex;">
