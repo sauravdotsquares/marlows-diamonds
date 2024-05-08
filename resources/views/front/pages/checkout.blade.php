ce42041eece6903b170d3f7bca0c8905f07287fa
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2024-05-08 18:10:22 +0530

    For changes the msg of aftercare prices

--- resources/views/front/pages/checkout.blade.php
+++ resources/views/front/pages/checkout.blade.php
@@ -457,7 +457,7 @@
                                                                 $checkStatus = 'checked'
                                                             @endphp
                                                         @endif
-                                                        <input type="checkbox" id="yearlySupport{{$id}}" name="yearlySupport" {{$checkStatus}} value="99"> Please check if you want yearly support
+                                                        <input type="checkbox" id="yearlySupport{{$id}}" name="yearlySupport" {{$checkStatus}} value="99"> Annual aftercare {{MY_CURRENCY_SYMBOL}} 99
                                                     @endif
                                                 </dl>
                                                 <strong class="checkpr-quantity">x {{$details['quantity']}}</strong>
