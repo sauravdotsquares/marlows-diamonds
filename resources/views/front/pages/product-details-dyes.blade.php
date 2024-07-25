b7db43c051d195ec86b625ee70fb35e62883b22b
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2024-07-25 15:40:13 +0530

    For removed price from product details schema

--- resources/views/front/pages/product-details-dyes.blade.php
+++ resources/views/front/pages/product-details-dyes.blade.php
@@ -1178,7 +1178,6 @@ $getFinalPrice = getMinimumPriceFunction($data);
     "@type": "Offer",
     "url": "{{url()->full()}}",
     "priceCurrency": "GBP",
-    "price": "{{isset($getFinalPrice['final_discounted_price'])?$getFinalPrice['final_discounted_price']:''}}",
     "availability": "https://schema.org/InStock",
     "itemCondition": "https://schema.org/NewCondition"
   },
