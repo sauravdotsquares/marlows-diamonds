a37cb731ce06cc30fbbf5281be8f61009e0a4843
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2024-07-30 18:51:20 +0530

    For remove comment from all over websites

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         'Seansch\Rapnet\RapnetServiceProvider',
+        App\Providers\BladeServiceProvider::class,
 
     ],
 
