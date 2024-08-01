94f83509311ec6fa00d5f463e5a9558ff85afaf7
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2024-08-01 17:57:57 +0530

    For Apply the metal type according half finger ring size prices

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -32,6 +32,7 @@ use App\Models\ProductThumbVideos;
 use App\Models\UrlRedirects;
 use App\Models\LabPricesList;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 
 //use SoapClient;
@@ -1799,7 +1800,7 @@ function getIpInfo($ip = NULL, $purpose = "location", $deep_detect = TRUE)
         $getDiscountedPrice = $getRegularPrices->shopPrice;
         $getFingerSizePrice = 0;
         if((isset($getProductDetails->product_parent_category) && $getProductDetails->product_parent_category == 47) && (strpos($getRequestData['fingersize'], '-1/2') !== false)){
-            $getFingerSizePrice = getFingerSizeHalfPrice();
+            $getFingerSizePrice = getFingerSizeHalfPrice($getRequestData['metal_type']);
         }
 
         $result = [
@@ -2288,8 +2289,12 @@ if (!function_exists("getEngagmentRingsLabPriceAdded")) {
     }
 }
 if (!function_exists("getFingerSizeHalfPrice")) {
-    function getFingerSizeHalfPrice()
+    function getFingerSizeHalfPrice($metalTypeVariation)
     {
-        return 35;
+        if(Str::contains($metalTypeVariation, '9ct') || Str::contains($metalTypeVariation, 'Silver')){
+            return 30;
+        }else{
+            return 50;
+        }
     }
 }
