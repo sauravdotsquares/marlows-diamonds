1918a2d9875a537bf6dacc3f000c244228d01ed2
Gajendra Sharma <sharma.gajendra@dotsquares.com>
2024-08-08 14:58:12 +0530

    For show product images in details pages

--- resources/views/front/includes/choosediamond.blade.php
+++ resources/views/front/includes/choosediamond.blade.php
@@ -44,12 +44,6 @@
                                                     
                                                     <div class="product-finder-price" id="finaldiamondprice"><span class="price">{{MY_CURRENCY_SYMBOL}} {{sprintf('%0.2f', $getProductListingPrices['final_discounted_price'])}} </span></div>
                                                 </div>
-                                                <?php 
-                                                    // echo "sdfdsfds<pre>";
-                                                    // print_r(sprintf('%0.2f', $getProductListingPrices['final_rrp_price']));
-                                                    // die;
-                                                
-                                                ?>
                                                 @if($getProductListingPrices['final_rrp_price'] != $getProductListingPrices['final_discounted_price'])
                                                     <p><span style="color:green">You Save : <span id="savePrice">{{MY_CURRENCY_SYMBOL}} {{ sprintf('%0.2f', $getProductListingPrices['final_rrp_price'] - $getProductListingPrices['final_discounted_price']) }}</span></span> |  <del id="rrpPrice">RRP: {{MY_CURRENCY_SYMBOL}} {{ sprintf('%0.2f', $getProductListingPrices['final_rrp_price']) }}</del> </p>
                                                 @endif
--- resources/views/front/pages/product-details-dno.blade.php
+++ resources/views/front/pages/product-details-dno.blade.php
@@ -533,7 +533,7 @@
 
 @section('js')
 	<script src="{{$url}}"></script>
-	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 	<script src="{{ asset('assets/vendors/fancybox-master/dist/jquery.fancybox.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
@@ -841,10 +841,10 @@
             });
 		}
 
-		toastr.options = {
-			"preventDuplicates": true,
-			"preventOpenDuplicates": true
-		};
+		// toastr.options = {
+		// 	"preventDuplicates": true,
+		// 	"preventOpenDuplicates": true
+		// };
 
 		function addtobasketFunction(getUrl,product_slug=null,index=null){
             var trdata = $('#finaldiamondprice .price').text().replace(/[^\0-9.-]+/g, '');
