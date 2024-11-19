26bc9bfefe2c69cb8dd2cb087874365489a5b5df
kartik-tanwar <kartik.tanwar@dotsquares.com>
2024-11-19 16:38:28 +0530

    update footer content

--- resources/views/layouts/front/footer.blade.php
+++ resources/views/layouts/front/footer.blade.php
@@ -87,8 +87,7 @@
      <div class="bottom-eye-icon">
         {{-- <a href="#"> --}}
            <img class="eye-num" src="{{asset('assets/images/wp-tooltip.png')}}" alt="whatsApp" class="position-realtive">
-           <span class="tooltiptext">10% Price beat service through Whatsapp. Send us a link of what you have seen elsewhere to get a minimum of 10% off competitors' price 
-            Only through whatsapp contact on any lab grown product</span>
+           <span class="tooltiptext">10% Price beat service through Whatsapp. Send us a link of what you have seen elsewhere to get a minimum of 10% off competitor's price. Only through whatsapp contact on any lab grown product</span>
        {{-- </a> --}}
     </div>
    {{-- eye icon --}}
@@ -96,7 +95,7 @@
        <div class="bottom-whatsapp-icon">
         <a class="whatspp-num" target="_blank" href="https://api.whatsapp.com/send?phone=447535425059">
             <img src="{{asset('assets/images/whatsapp.png')}}" alt="whatsApp" class="position-realtive">
-            <span class="tooltiptext">10% Price beat service through Whatsapp. Send us a link of what you have seen elsewhere to get a minimum of 10% off competitors' price Only through whatsapp contact on any lab grown product</span>
+            <span class="tooltiptext">10% Price beat service through Whatsapp. Send us a link of what you have seen elsewhere to get a minimum of 10% off competitor's price. Only through whatsapp contact on any lab grown product</span>
         </a>
     </div>
     {{-- whatsapp icon --}}
