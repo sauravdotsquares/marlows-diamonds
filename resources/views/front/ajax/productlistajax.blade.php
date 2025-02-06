5d1e11621170e9d9d5a78110372324bd5e6e2ef3
kartik-tanwar <kartik.tanwar@dotsquares.com>
2025-02-06 19:13:10 +0530

    removed metal color section

--- resources/views/front/ajax/productlistajax.blade.php
+++ resources/views/front/ajax/productlistajax.blade.php
@@ -145,14 +145,14 @@ $thumbnailGif = getThumbnailGif($product->id); ?>
 
 
 
-		<div class="color-buttons">
+		{{-- <div class="color-buttons">
 
 			@if (stripos($product->title, 'engagement ring') === false)
 
 
 			<a class="color-default" id="fetchdefaultimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="Default">Default</a>
 			
-			{{-- <a class="color-btn silver" id="fetchvariationSilverimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="Silver">Silver</a> --}}
+			<a class="color-btn silver" id="fetchvariationSilverimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="Silver">Silver</a>
 
 
 
@@ -160,7 +160,7 @@ $thumbnailGif = getThumbnailGif($product->id); ?>
 			<a class="color-btn rose-gold" id="fetchvariationRoseimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="18ct Rose Gold">Rose Gold</a>
 			<a class="color-btn yellow-gold" id="fetchvariationYellowimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="18ct Yellow Gold">Yellow Gold</a>
 			@endif
-		</div>
+		</div> --}}
 
 
 
--- resources/views/front/pages/product_listing_page.blade.php
+++ resources/views/front/pages/product_listing_page.blade.php
@@ -798,20 +798,20 @@ if(in_array('diamond-engagement-rings',$pathData) || in_array('engagement-rings'
                                         </div>
 
 
-                                        <div class="color-buttons">
+                                        {{-- <div class="color-buttons">
                                             @if (stripos($product->title, 'engagement ring') === false)
 
                                             <a class="color-default" id="fetchdefaultimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="Default">Default</a>
 
 
 
-                                            {{-- <a class="color-btn silver" id="fetchvariationSilverimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="Silver">Silver</a> --}}
+                                            <a class="color-btn silver" id="fetchvariationSilverimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="Silver">Silver</a>
 
 
                                             <a class="color-btn rose-gold" id="fetchvariationRoseimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="18ct Rose Gold">Rose Gold</a>
                                             <a class="color-btn yellow-gold" id="fetchvariationYellowimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="18ct Yellow Gold">Yellow Gold</a>
                                             @endif
-                                        </div>
+                                        </div> --}}
 
                                         
                                         <div class="product-items-item-details">
