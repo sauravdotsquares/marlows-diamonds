7dc04416f5482030b47030cd9fde58969f5c6c71
kartik-tanwar <kartik.tanwar@dotsquares.com>
2025-02-14 19:14:44 +0530

    removed condition

--- resources/views/front/pages/product_listing_page.blade.php
+++ resources/views/front/pages/product_listing_page.blade.php
@@ -801,12 +801,12 @@ if(in_array('diamond-engagement-rings',$pathData) || in_array('engagement-rings'
                                          <div class="color-buttons">
                                             {{-- @if (stripos($product->title, 'engagement ring') === false) --}}
 
-                                            <a class="color-default" id="fetchdefaultimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="Default"  data-src="{{ env('APP_IMAGE_URL') . '/storage/' . $product->getProductImages['image_url'] }}" >Default</a>
+                                            {{-- <a class="color-default" id="fetchdefaultimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="Default"  data-src="{{ env('APP_IMAGE_URL') . '/storage/' . $product->getProductImages['image_url'] }}" >Default</a> --}}
 
                                             {{-- <a class="color-btn silver" id="fetchvariationSilverimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="Silver">Silver</a> --}}
 
-                                            <a class="color-btn rose-gold" id="fetchvariationRoseimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="18ct Rose Gold">Rose Gold</a>
-                                            <a class="color-btn yellow-gold" id="fetchvariationYellowimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="18ct Yellow Gold">Yellow Gold</a>
+                                            {{-- <a class="color-btn rose-gold" id="fetchvariationRoseimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="18ct Rose Gold">Rose Gold</a>
+                                            <a class="color-btn yellow-gold" id="fetchvariationYellowimages{{ $product->id }}" data-slug="{{ $product->slug }}" data-color="18ct Yellow Gold">Yellow Gold</a> --}}
                                             {{-- @endif --}}
                                         </div> 
 
