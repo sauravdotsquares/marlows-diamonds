867f1a255043848eb3cd3139f9c7db07d90e2cf9
kartik-tanwar <kartik.tanwar@dotsquares.com>
2025-03-31 18:49:11 +0530

    updated text

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -25,7 +25,7 @@
             <div class="main-banner-col banner-left-col">
                 <div class="main-banner-left-text">
                     <h1 class="123">GIA Certified Diamond Rings by Marlows</h1>
-                    <p><strong>Over 2500 mined and lab grown diamonds in stock to take away at online prices</strong></p>
+                    <p><strong>Over 2500 mined and lab grown diamonds in stock</strong></p>
                     <div class="shop-engage-btn">
                     <a class="btn-bg-large" href="{{ asset('diamond-engagement-rings') }}">SHOP ENGAGEMENT RINGS</a>
                     </div>
