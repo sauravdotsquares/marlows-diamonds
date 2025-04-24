93ad36a9a52f9477acf6b866e0c353acac21e841
kartik-tanwar <kartik.tanwar@dotsquares.com>
2025-04-24 12:15:17 +0530

    added alt tag

--- resources/views/layouts/front/seo_header.blade.php
+++ resources/views/layouts/front/seo_header.blade.php
@@ -51,7 +51,7 @@
         fbq('track', 'PageView');
     </script>
     <noscript>
-        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1238805530482186&ev=PageView&noscript=1"/>
+        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1238805530482186&ev=PageView&noscript=1" alt="facebook"/>
     </noscript>
     <!-- End Meta Pixel Code -->
 @endif
