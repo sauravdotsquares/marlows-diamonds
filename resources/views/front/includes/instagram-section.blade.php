798f330d45d84072f1ab1833605536e03574589c
kartik-tanwar <kartik.tanwar@dotsquares.com>
2025-07-05 14:22:22 +0530

    added instagram updated images

--- resources/views/front/includes/instagram-section.blade.php
+++ resources/views/front/includes/instagram-section.blade.php
@@ -3,24 +3,113 @@ $getInstaData = getInstagramDataDetails();
 ?>
 @if(count($getInstaData) > 0)
 <div class="share-moment">
-    <div class="share-moment-heading"><div class="heading-h-two">Share your special moments with us<br><span> #marlowsengagements</span></div></div>
+    <div class="share-moment-heading">
+        <div class="heading-h-two">
+            Share your special moments with us<br><span> #marlowsengagements</span>
+        </div>
+    </div>
     <div class="insta-photo-list">
         <div class="owl-carousel owl-theme photo-slider">
-            @foreach($getInstaData as $key => $insta)
-                <div class="item">
-                    <div class="instaphoto-info">
-                         <div class="instaphoto-img">
-                            <?php 
-                                $instagramImageUrl = getImageOptimizeDetails('/images/'.$insta->image_url,'256','370');
-                            ?>
-                            <img src="{{ $instagramImageUrl }}" alt="{{isset($insta->alt)?$insta->alt:'GIA Certified Diamond Jewellery Supplier | London and Birmingham'}}" loading="lazy">
-                        </div> 
-                        <div class="insta-link">
-                            <a href="{{$insta->link}}" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                        </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta1.jpg" alt="couple" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DLpMDMlIeIU/" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                     </div>
                 </div>
-            @endforeach
+            </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta2.jpg" alt="passion love" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DLjaaTJofec/" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta3.jpg" alt="round cut shoulders" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DLe535qoW1K" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta4.jpg" alt="necklace" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DLb_4gtIjza" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta5.jpg" alt="princess cut" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DLZMsJbo8Yf/" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta6.jpg" alt="gift" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DLSc6qIoTeB/" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta7.jpg" alt="ring" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DLIJnuBIJXv/" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta8.jpg" alt="necklace" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DKq3dy3IsoC/" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta9.jpg" alt="complimentary gift" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DKhVr2fI86h/" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="instaphoto-info">
+                    <div class="instaphoto-img">
+                        <img src="/images/insta/insta10.jpg" alt="hello june" loading="lazy">
+                    </div>
+                    <div class="insta-link">
+                        <a href="https://www.instagram.com/p/DKWOqItINjx/" target="_blank" rel="follow"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div class="insta-btn" id="getdirection">
