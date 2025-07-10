014cebc8cfa784198bd1e8149f4a77ac2e1dd8fe
kartik-tanwar <kartik.tanwar@dotsquares.com>
2025-07-10 11:37:54 +0530

    added minified file for css

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -33,7 +33,7 @@
     <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/developer.css?').env('VERSION') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/responsive.min.css') }}" rel="stylesheet">
 
