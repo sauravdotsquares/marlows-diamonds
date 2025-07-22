85ad26d80230181449a6ca877add6791bcc99c31
kartik-tanwar <kartik.tanwar@dotsquares.com>
2025-07-22 11:49:01 +0530

    added compressed image poster of video

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -356,7 +356,7 @@
                         skilled polishers focus on maximising proportions rather than just carat weight, meaning our 1ct diamonds often look equivalent to a 1.25ct from other jewellers. We invite you to visit any
                         of our stores to be amazed. Furthermore, if you find a better price elsewhere, simply send us a link, and we will gladly
                         beat it—guaranteeing you the best deal.</p>
-                    <video  id="video" autoplay muted loop playsinline  poster="/storage/HomePageVideos/homeopagevideo.png">
+                    <video  id="video" autoplay muted loop playsinline  poster="/storage/HomePageVideos/homeopagevideo.webp">
                     <source src="{{ env('APP_IMAGE_URL') . '/storage/HomePageVideos/homeopagevideo.mp4' }}" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
