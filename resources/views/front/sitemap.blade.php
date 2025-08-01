85021a6b6e544d8877af15f1dbc7d7f3274b3863
kartik-tanwar <kartik.tanwar@dotsquares.com>
2025-08-01 11:17:41 +0530

    updated SEO points for the sitemaps

--- resources/views/front/sitemap.blade.php
+++ resources/views/front/sitemap.blade.php
@@ -1,7 +1,7 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-
+ 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    
+   
 {{-- All other pages --}}
     @foreach ($otherPages as $key => $otherPage)
         @if($key == 'homepage')
@@ -18,53 +18,56 @@
             </url>
         @endif
     @endforeach
-
+ 
     {{-- All other pages --}}
-    
+   
     @foreach ($categoryUrlsList as $categoryUrlPages)
+        @if ($loop->first)
+            @continue
+        @endif
         <url>
-            <loc>{{ rtrim(env('APP_ROOT_URL') .  $categoryUrlPages['url'] , '/') }}</loc>
-            <lastmod>{{ Carbon\Carbon::parse($categoryUrlPages['updated_at'])->tz('UTC')->toAtomString() }}</lastmod>
+            <loc>{{ rtrim(env('APP_ROOT_URL') . $categoryUrlPages['url'], '/') }}</loc>
+            <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
             <priority>1.0</priority>
         </url>
     @endforeach
-
+ 
     {{-- Products --}}
     @foreach ($products as $product)
         <url>
             <loc>{{  rtrim(env('APP_ROOT_URL') . '/product/'. $product->slug,'/') }}</loc>
-            <lastmod>{{ $product->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+            <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.8</priority>
         </url>
     @endforeach
-    
+   
     {{-- Posts --}}
     @foreach ($posts as $post)
         <url>
             <loc>{{  rtrim(env('APP_ROOT_URL') . '/blog/' . $post->slug,'/') }}</loc>
-            <lastmod>{{ $post->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+            <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.8</priority>
         </url>
     @endforeach
-
+ 
     {{-- pages --}}
     @foreach ($pages as $page)
         <url>
             <loc>{{ rtrim(env('APP_ROOT_URL') .'/' .  $page->slug,'/') }}</loc>
-            <lastmod>{{ $page->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+            <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.8</priority>
         </url>
     @endforeach
-
+ 
     {{-- posts_category --}}
-    @foreach ($posts_categories as $posts_category)
+    {{-- @foreach ($posts_categories as $posts_category)
         <url>
             <loc>{{  rtrim(env('APP_ROOT_URL') . '/blog/category/' . $posts_category->slug,'/') }}</loc>
             <lastmod>{{ $posts_category->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <priority>0.7</priority>
         </url>
-    @endforeach
-
-    
-
+    @endforeach --}}
+ 
+   
+ 
 </urlset>
